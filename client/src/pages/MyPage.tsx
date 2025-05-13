392320ff00e68916ddfc9386e86776618a20bc15
김시은 <tldms3241@gmail.com>
2025-05-13 17:44:23 +0900

    feat : header 추가

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -1,4 +1,5 @@
 import NavBar from 'components/NavBar';
+import Header from 'features/header/Header';
 import LogoutButton from 'features/myPage/LogoutButton';
 import Profile from 'features/myPage/Profile';
 import JoinGroupList from 'features/myPage/JoinGroupList';
@@ -6,6 +7,7 @@ import JoinGroupList from 'features/myPage/JoinGroupList';
 const MyPage = () => {
 	return (
 		<div>
+			<Header title="마이페이지" showLogo={false} />
 			<Profile />
 			<JoinGroupList />
 			<LogoutButton />
