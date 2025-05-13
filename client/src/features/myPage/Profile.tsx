bbe89e2fddd5d0d82ac8e36b4c842fee066804c4
김시은 <tldms3241@gmail.com>
2025-05-13 18:19:00 +0900

    add : 파일 추가

--- client/src/features/myPage/Profile.tsx
+++ client/src/features/myPage/Profile.tsx
@@ -1,3 +1,9 @@
 import './Profile.scss';
 import 'assets/style/_flex.scss';
 import 'assets/style/_typography.scss';
+
+function Profile() {
+	return <div>Profile</div>;
+}
+
+export default Profile;
