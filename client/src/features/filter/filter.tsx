bfd52619ee23ee78f0bdbda94349b008051a608e
김시은 <tldms3241@gmail.com>
2025-05-21 16:06:52 +0900

    feat : filter ui 구현

--- client/src/features/filter/filter.tsx
+++ client/src/features/filter/filter.tsx
@@ -1,9 +1,27 @@
-import './filter.scss';
 import 'assets/style/_flex.scss';
 import 'assets/style/_typography.scss';
+import React from 'react';
+import './filter.scss';
 
-function filter() {
-	return <div>filter</div>;
-}
+const Filter: React.FC = () => {
+	return (
+		<div className="filter-container flex-center">
+			<div className="filter flex-left">
+				<button type="button" className="filter-button dropdown button2">
+					만남장소
+				</button>
+				<button type="button" className="filter-button dropdown button2">
+					만남횟수
+				</button>
+				<button type="button" className="filter-button dropdown button2">
+					시간대
+				</button>
+				<button type="button" className="filter-button dropdown button2">
+					분야
+				</button>
+			</div>
+		</div>
+	);
+};
 
-export default filter;
+export default Filter;
