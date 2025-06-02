722d5be3ddabc77e05c0a963b8da2dabf71b4551
김시은 <tldms3241@gmail.com>
2025-06-03 00:54:35 +0900

    add : 파일 추가 및 변경

--- client/src/utils/profileImageMap.ts
+++ client/src/utils/profileImageMap.ts
@@ -1,10 +1,9 @@
 // 마이페이지
 export const userProfileImageMap: Record<number, string> = {
-	1: '/assets/img/profile/profile-1st.jpg',
-	2: '/assets/img/profile/profile-2nd.jpg',
-	3: '/assets/img/profile/profile-3rd.jpg',
-	4: '/assets/img/profile/profile-default.jpg',
-	5: '/assets/img/profile/profile-non-attendance.jpg',
+	1: '/assets/img/profile/mydolearn-1.png',
+	2: '/assets/img/profile/mydolearn-2.png',
+	3: '/assets/img/profile/mydolearn-3.png',
+	4: '/assets/img/profile/mydolearn-4.png',
 };
 
 export const getUserProfileImageUrl = (imageId: number): string => {
@@ -13,11 +12,11 @@ export const getUserProfileImageUrl = (imageId: number): string => {
 
 // 그룹 멤버 리스트, 랭킹
 export const groupMemberImageMap: Record<number, string> = {
-	1: '/assets/img/profile/profile-1st.jpg',
-	2: '/assets/img/profile/profile-2nd.jpg',
-	3: '/assets/img/profile/profile-3rd.jpg',
-	4: '/assets/img/profile/profile-default.jpg',
-	5: '/assets/img/profile/profile-non-attendance.jpg',
+	1: '/assets/img/ranking/profile-1st.jpg',
+	2: '/assets/img/ranking/profile-2nd.jpg',
+	3: '/assets/img/ranking/profile-3rd.jpg',
+	4: '/assets/img/ranking/profile-default.jpg',
+	5: '/assets/img/ranking/profile-non-attendance.jpg',
 };
 
 export const getGroupMemberProfileImageUrl = (imageId: number): string => {
