c4b56f8417b20d8d443a81038639fbc7354e2e1a
김시은 <tldms3241@gmail.com>
2025-06-03 00:05:16 +0900

    fix : 파일 변경에 따른 매핑 수정

--- client/src/utils/profileImageMap.ts
+++ client/src/utils/profileImageMap.ts
@@ -1,9 +1,9 @@
 export const profileImageMap: Record<number, string> = {
-	1: '/assets/profile_images/profile-1st.png',
-	2: '/assets/profile_images/profile-2nd.png',
-	3: '/assets/profile_images/profile-3rd.png',
-	4: '/assets/profile_images/profile-default.png',
-	5: '/assets/profile_images/profile-non-attendance.png',
+	1: '/assets/profile_images/profile-1st.jpg',
+	2: '/assets/profile_images/profile-2nd.jpg',
+	3: '/assets/profile_images/profile-3rd.jpg',
+	4: '/assets/profile_images/profile-default.jpg',
+	5: '/assets/profile_images/profile-non-attendance.jpg',
 };
 
 export const getProfileImageUrl = (imageId: number): string => {
