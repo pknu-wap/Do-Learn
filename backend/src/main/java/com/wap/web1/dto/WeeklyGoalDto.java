339fc8867f3d36e1b62a23d37e2079c57e5f184e
chaerin05 <lcl8661@naver.com>
2025-06-04 18:32:51 +0900

    fix:goalDto도 기본생성자 추가

--- backend/src/main/java/com/wap/web1/dto/WeeklyGoalDto.java
+++ backend/src/main/java/com/wap/web1/dto/WeeklyGoalDto.java
@@ -1,13 +1,17 @@
 package com.wap.web1.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class WeeklyGoalDto {
     private Long goalId;
